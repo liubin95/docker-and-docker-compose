f7320139921998a0dc05fb792b0cd214e4123dbf
liubin <liu_bin4835@outlook.com>
2023-03-10 17:06:37 +0800

    count command

--- sh/count-comand.sh
+++ sh/count-comand.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Count the number of commands in history
-grep -E '^:' 0309 | awk -F ';' '{print $2}' | sort | uniq -c | sort -rn
-grep -E '^:' 0309 | awk -F ';' '{print $2}' | awk '{print $1}' | sort | uniq -c | sort -rn
+grep -E '^:' ~/.zsh_history | awk -F ';' '{print $2}' | sort | uniq -c | sort -rn
+grep -E '^:' ~/.zsh_history | awk -F ';' '{print $2}' | awk '{if($1 == "sudo"){print $2}else{print $1}}' | sort | uniq -c | sort -rn
