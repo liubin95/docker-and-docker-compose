ff3ff7b70e59d9286451ba2b2c5a71f02f6f8353
liubin <liu_bin4835@outlook.com>
2023-03-23 17:50:01 +0800

    允许跨域请求 update

--- python/weather/main.py
+++ python/weather/main.py
@@ -66,7 +66,6 @@ app = FastAPI(docs_url=None, redoc_url=None)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
