b18a89647cb84e08618a1022bfd26433acdc9dd8
liubin <liu_bin4835@outlook.com>
2023-04-04 16:58:02 +0800

    move linux

--- sh/count-comand.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Count the number of commands in history
-grep -E '^:' ~/.zsh_history | awk -F ';' '{print $2}' | sort | uniq -c | sort -rn
-grep -E '^:' ~/.zsh_history | awk -F ';' '{print $2}' | awk '{if($1 == "sudo"){print $2}else{print $1}}' | sort | uniq -c | sort -rn
