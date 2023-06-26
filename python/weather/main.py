aced62fc2bdaaa7c9fafd848d8e814f47ece3f7c
liubin <liu_bin4835@outlook.com>
2023-06-26 16:35:09 +0800

    update main.py

--- python/weather/main.py
+++ python/weather/main.py
@@ -87,7 +87,6 @@ async def add_process_time_header(request: Request, call_next):
     response.headers["X-Process-Time"] = str(process_time)
     return response
 
-i
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
